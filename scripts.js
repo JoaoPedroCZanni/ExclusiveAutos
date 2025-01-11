f832fa23b9d2dae4fa4561e2fa402a5481b0b7ed
yZanni <164171833+JoaoPedroCZanni@users.noreply.github.com>
2025-01-11 11:59:48 -0300

    Implementação do botão Next funcional

--- scripts.js
+++ scripts.js
@@ -5,8 +5,15 @@ let items = container.querySelectorAll('.list .item')
 let indicator = document.querySelector('.indicators')
 let dots = indicator.querySelectorAll('ul li')
 
+
+
+let active = 0 
+let firstPosition = 0 
+let lastPosition = items.length - 1
+
 nextButton.onclick = () => {
-    console.log("Botão next ")
+    let itemOld = container.querySelector('.list .item.active')
+    itemOld.classList.remove('active')
 }
 
 prevButton.onclick = () => {
