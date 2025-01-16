d3022269002a788b3350c531801865f1a26aa1ee
yZanni <164171833+JoaoPedroCZanni@users.noreply.github.com>
2025-01-16 11:47:15 -0300

    Mudança do lado que as imagens vão aparecendo conforme o botão clicado

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ let container = document.querySelector('.container')
 let items = container.querySelectorAll('.list .item')
 let indicator = document.querySelector('.indicators')
 let dots = indicator.querySelectorAll('ul li')
-
+let list = container.querySelector('.list')
 
 
 let active = 0
@@ -23,6 +23,7 @@ function setSlider() {
 }
 
 nextButton.onclick = () => {
+    list.style.setProperty('--calculation', 1)
     active = active + 1 > lastPosition ? 0 : active + 1
     setSlider()
     items[active].classList.add('active')
@@ -30,6 +31,7 @@ nextButton.onclick = () => {
 
 
 prevButton.onclick = () => {
+    list.style.setProperty('--calculation', -1)
     active = active - 1 < firstPosition ? lastPosition : active - 1
     setSlider()
     items[active].classList.add('active')
